be8df645f68e6f312673642ffb9d02f40cc02e1b
Sebastian <sebaa08@gmail.com>
2018-12-08 13:03:20 -0300

    comments

--- uala-movies/src/main/java/com/uala/uala_movies/App.java
+++ uala-movies/src/main/java/com/uala/uala_movies/App.java
@@ -4,10 +4,30 @@ package com.uala.uala_movies;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		/*
+		 * # uala-movies
+		 * 
+		 * Observaciones:
+		 * 
+		 * Detallo algunas clases particulares que cumplen las funciones pedidas.
+		 * 
+		 * UalaMoviesUserAnalyzer: Saber si un cliente mira algo interesante
+		 * UalaMoviesBilling: Efectuar cobro por parte de Uala-movies UalaMoviesLogin:
+		 * Hacer la funcionalidad para que el usuario entre al sistema.
+		 * UalaProductRecomendator: Poder pedirle recomendaciones al sistema.
+		 * 
+		 * 
+		 * 
+		 * Patrones de diseño: Utilice el patron singleton para la creacion del
+		 * recomendador de productos. Se pueden utilizar mas pero creo que el diseño
+		 * planteado esta bastante correcto para la consigna dada.
+		 * 
+		 * 
+		 * Puntos no resueltos (por tiempo) Realizar el unit testing. Especificar
+		 * patrones de diseño en c aso de haber utilizado y dónde se usaron. Diagrama de
+		 * clases de la solución planteada.
+		 */
+	}
 }
